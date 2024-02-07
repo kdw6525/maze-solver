ce84080297a331b6a02fb2ff703587008470ead6
kdw6525 <kdw6525@rit.edu>
2024-02-06 19:45:29 -0500

    commit maze_solver.py

---
+++ maze_solver.py
@@ -0,0 +1,27 @@
+# maze_solver.py
+#
+# solves No right turns! maze using bfs, dfs and a modified search algorithm
+# 
+# python maze_solver.py maze 
+#
+
+def read_maze(files):
+    
+    return
+
+def dfs():
+    return
+
+def bfs():
+    return
+
+def modified_search():
+    return
+
+def main():
+
+    return
+
+
+if __name__ == '__main__':
+    main()
