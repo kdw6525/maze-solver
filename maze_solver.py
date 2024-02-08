a3fe6ca06e1fbba2b994013e59c3d8b0b2d34ee0
Kyle West <kdw6525@rit.edu>
2024-02-07 23:02:38 -0500

    removed spaces in maze1.txt used regex to read maze lines in maze_solver.py

--- maze_solver.py
+++ maze_solver.py
@@ -5,9 +5,25 @@
 # python maze_solver.py maze 
 #
 
-def read_maze(files):
+import re
+
+def read_maze(file):
+    # read file into graph like structure
+    # index is vertex, value is edges (edges is an array of tuples)
+    # tuples are (neighbor, direction)
+    # neighbors are the index and direction is 0, 1, 2, and 3
+    #                                         (N, E, S, and W)
+
+    lines = file.readlines()
+    maze = []
+    for line in lines:
+        data = re.match('(-?[0-9]*) ([\(-?[0-9]*,[0-4]\)(,\(-?[0-9]*,[0-4]\))*])( g)?', line)
+        v, e, g = data[1], data[2], data[4]
+        print(v)
+        maze.append((e, g))
+
     
-    return
+    return 
 
 def dfs():
     return
@@ -19,7 +35,8 @@ def modified_search():
     return
 
 def main():
-
+    file = open('maze1.txt')
+    read_maze(file)
     return
 
 
